db57919186e61fea7033e3e30847652e3bbf9bf0
saquibsayyed <127868412+saquibsayyedcoder@users.noreply.github.com>
2024-01-27 17:46:25 +0530

    Add files via upload
    E:\React class\02counter\src\App.jsx

---
+++ App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let[counter, setCounter] = useState(15)
+
+
+
+  const addValue = () =>{
+    setCounter(counter + 1)
+  }
+
+  const removeValue = () =>{
+    setCounter(counter - 1)
+  }
+
+
+ 
+  return (
+    <>
+    <h1>Learn React</h1>
+    <h2>Counter Value: {counter}</h2>
+
+  <button onClick={addValue}>
+    Add Value</button>
+    <br />
+    <button onClick={removeValue}>
+      Remove Value</button>     
+    </>
+  )
+}
+
+export default App
